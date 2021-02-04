709fdb3d551b494c832cb4c638af6c90e62ffe91
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-02-04 14:19:26 +0530

    write questions solection function

--- src/App.js
+++ src/App.js
@@ -5,20 +5,94 @@ const axios = require('axios');
 class App extends React.Component {
   state = {
     countries : [],
-    lives : 3
+    lives : 3,
+    questions : [],
+    currentQuestion : 0,
+    score : 0
   }
+
   componentDidMount() {
     axios.get('https://restcountries.eu/rest/v2/all?fields=name;capital;flag')
     .then(res => {
       this.setState({
         countries: res.data
-      })
-    });
+      });
+    }).then(() => {
+      this.initializeQuestions();
+    })
   }
+
+  shuffle = (arr) => {
+    const len = arr.length;
+    for(var i=0; i<len; i++) {
+      const idx = Math.floor(Math.random() * len);
+      let temp = arr[idx];
+      arr[idx] = arr[i];
+      arr[i] = temp;
+    }
+  }
+
+  initializeQuestions = () => {
+    let questions = [], selectedCountries = [];
+    while(questions.length < 5) {
+      let idx = Math.floor((Math.random()*this.state.countries.length));
+      if(!selectedCountries.find(el => el === idx)) {
+        selectedCountries.push(idx);
+        const country = this.state.countries[idx];
+        let options = [];
+        options.push(country.name);
+        let temp = idx;
+        for(let i=0; i<3; i++) {
+            temp+=60;
+            temp%=250;
+            let option = this.state.countries[temp].name;
+            options.push(option);
+        }
+        this.shuffle(options);
+        const question = {
+          questionStatement : `${country.capital} is the capital of`, 
+          correctAnswer : country.name, 
+          options
+        }
+        questions.push(question);
+      }
+    }
+    while(questions.length < 10) {
+      let idx = Math.floor((Math.random()*this.state.countries.length));
+      if(!selectedCountries.find(el => el === idx)) {
+        selectedCountries.push(idx);
+        const country = this.state.countries[idx];
+        const correctAnswer = country.name;
+        let options = [];
+        options.push(correctAnswer);
+        let temp = idx;
+        for(let i=0; i<3; i++) {
+            temp+=60;
+            temp%=250;
+            let option = this.state.countries[temp].name;
+            options.push(option);
+        }
+        this.shuffle(options);
+        const question = {
+          questionStatement : 'The shown flag belongs to', 
+          flag : country.flag,
+          correctAnswer, 
+          options
+        }
+        questions.push(question);
+      }
+    }
+    this.shuffle(questions);
+    this.setState({
+      questions
+    });
+    console.log(this.state.questions);
+  }
+
   render() {
     return (
       <div className='main'>
-        <QuizCard lives={this.state.lives} />
+        <QuizCard lives = {this.state.lives} question = {this.state.questions[this.state.currentQuestion]} />
       </div>
     )
   }
--- src/components/quiz-card/quiz-card.jsx
+++ src/components/quiz-card/quiz-card.jsx
@@ -10,10 +10,10 @@ class QuizCard extends React.Component {
     componentDidMount() {
         let livesArr = [];
         for(var i=0; i<this.props.lives; i++) {
-            livesArr.push(1);
+            livesArr.push(i);
         }
         this.setState({
-            lives : livesArr
+            lives : livesArr, 
         })
     }
 
@@ -46,7 +46,7 @@ class QuizCard extends React.Component {
                             {
                                 this.state.lives.map(num => {
                                     return (
-                                        <img className='heart' src="https://www.flaticon.com/svg/vstatic/svg/1489/1489937.svg?token=exp=1612425025~hmac=56a0d7a1d4e264bb6d46dd7df944820b" alt="heart"/>
+                                        <img key={num} className='heart' src="https://www.flaticon.com/svg/vstatic/svg/1489/1489937.svg?token=exp=1612425025~hmac=56a0d7a1d4e264bb6d46dd7df944820b" alt="heart"/>
                                     )
                                 })
                             }
